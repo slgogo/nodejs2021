c0cae3be1b54fc0d61b595ae5bcda533eeecb3e2
slgogo <craneweb@qq.com>
2021-03-18 15:00:58 +0800

    http项目结束提交代码

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 const port =3000;
+
+/*使用JSON中间件*/
+app.use(express.json());
+
 app.listen(port,()=>{
     console.log('express服务已启动！');
 });
@@ -36,4 +40,27 @@ app.get('/posts/:postId',(request,response)=>{
     const posts = data.filter(item=>item.id==postId);
 
     response.send(posts[0]);
+});
+
+/*
+创建内容
+*/
+app.post('/posts',(request,response)=>{
+    //获取请求里的链接
+    const{content}=request.body;
+
+
+    //设置响应状态码
+    response.status(201);
+
+    //输出请求头部数据
+    console.log(request.headers['sing-along']);
+
+    //设置服务端响应数据
+    response.set('Sing-Along','This is a texture!');
+
+    //做出相应
+    response.send({
+        message:`成功创建了内容：${content}`
+    });
 });
